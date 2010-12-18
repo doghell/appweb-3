440f2119b344748071a69cacb08cfc62037691d6
Embedthis Software <dev@embedthis.com>
2010-12-17 20:17:12 -0800

    Cleanup

--- src/http/handlers/phpHandler.c
+++ src/http/handlers/phpHandler.c
@@ -1,7 +1,7 @@
 /*
- *  phpHandler.c - Appweb PHP handler
- *
- *  Copyright (c) All Rights Reserved. See details at the end of the file.
+    phpHandler.c - Appweb PHP handler
+  
+    Copyright (c) All Rights Reserved. See details at the end of the file.
  */
 
 /********************************** Includes **********************************/
@@ -243,8 +243,6 @@ static void runPhp(MaQueue *q)
 #endif
 
     zend_try {
-        // CG(start_lineno) = 7;
-        // OG(output_start_lineno) = 7;
         php_execute_script(&file_handle TSRMLS_CC);
         if (!SG(headers_sent)) {
             sapi_send_headers(TSRMLS_C);
@@ -262,7 +260,7 @@ static void runPhp(MaQueue *q)
     maPutForService(q, maCreateEndPacket(q), 1);
 }
 
- /*************************** PHP Support Functions ***************************/
+/*************************** PHP Support Functions ***************************/
 /*
  *
  *  Flush write data back to the client
@@ -508,31 +506,31 @@ MprModule *maPhpHandlerInit(MaHttp *http, cchar *path)
 #endif /* BLD_FEATURE_PHP */
 
 /*
- *  @copy   default
- *
- *  Copyright (c) Embedthis Software LLC, 2003-2010. All Rights Reserved.
- *  Copyright (c) Michael O'Brien, 1993-2010. All Rights Reserved.
- *
- *  This software is distributed under commercial and open source licenses.
- *  You may use the GPL open source license described below or you may acquire
- *  a commercial license from Embedthis Software. You agree to be fully bound
- *  by the terms of either license. Consult the LICENSE.TXT distributed with
- *  this software for full details.
- *
- *  This software is open source; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the
- *  Free Software Foundation; either version 2 of the License, or (at your
- *  option) any later version. See the GNU General Public License for more
- *  details at: http://www.embedthis.com/downloads/gplLicense.html
- *
- *  This program is distributed WITHOUT ANY WARRANTY; without even the
- *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- *  This GPL license does NOT permit incorporating this software into
- *  proprietary programs. If you are unable to comply with the GPL, you must
- *  acquire a commercial license to use this software. Commercial licenses
- *  for this software and support services are available from Embedthis
- *  Software at http://www.embedthis.com
- *
- *  @end
+    @copy   default
+  
+    Copyright (c) Embedthis Software LLC, 2003-2010. All Rights Reserved.
+    Copyright (c) Michael O'Brien, 1993-2010. All Rights Reserved.
+  
+    This software is distributed under commercial and open source licenses.
+    You may use the GPL open source license described below or you may acquire
+    a commercial license from Embedthis Software. You agree to be fully bound
+    by the terms of either license. Consult the LICENSE.TXT distributed with
+    this software for full details.
+  
+    This software is open source; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version. See the GNU General Public License for more
+    details at: http://www.embedthis.com/downloads/gplLicense.html
+  
+    This program is distributed WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  
+    This GPL license does NOT permit incorporating this software into
+    proprietary programs. If you are unable to comply with the GPL, you must
+    acquire a commercial license to use this software. Commercial licenses
+    for this software and support services are available from Embedthis
+    Software at http://www.embedthis.com
+  
+    @end
  */
