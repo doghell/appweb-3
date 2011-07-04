05480ffd7e08d37a3384b39243c89e225152fbd4
Embedthis Software <dev@embedthis.com>
2011-07-03 14:13:50 -1000

    Refine SendFileToSocket

--- src/mpr/mprLib.c
+++ src/mpr/mprLib.c
@@ -21761,9 +21761,9 @@ MprOffset mprSendFileToSocket(MprSocket *sock, MprFile *file, MprOffset offset, 
 #endif
             if (rc > 0) {
                 written += rc;
-                if (rc != toWriteFile) {
-                    done++;
-                }
+            }
+            if (rc != toWriteFile) {
+                done++;
             }
         }
         if (!done && afterCount > 0) {
@@ -21773,7 +21773,6 @@ MprOffset mprSendFileToSocket(MprSocket *sock, MprFile *file, MprOffset offset, 
             }
         }
     }
-
     if (rc < 0) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
             return written;
