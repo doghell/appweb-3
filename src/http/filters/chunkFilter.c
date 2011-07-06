e58880cb6a411b7f1bfdf8cfd885076d582bed30
Embedthis Software <dev@embedthis.com>
2011-07-05 22:02:31 -1000

    Bypass chunking for large static files

--- src/http/filters/chunkFilter.c
+++ src/http/filters/chunkFilter.c
@@ -168,7 +168,7 @@ static void outgoingChunkService(MaQueue *q)
                 resp->length = q->count;
             }
         } else {
-            if (resp->chunkSize < 0) {
+            if (resp->chunkSize < 0 && resp->entityLength < 0) {
                 resp->chunkSize = (int) min(conn->http->limits.maxChunkSize, q->max);
             }
         }
